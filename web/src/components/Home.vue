5e352825fff8130084fa4d1c0b5ef582899d736c
Josh Humphries <j.humphries@nhm.ac.uk>
2024-04-26 10:09:36 +0100

    feat: add an image to the homepage and some centred text
    Placeholder for the moment, need to find out what Ben wants.

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Home!</h1>
+  <div
+    class="bg-[url('/images/homebg.jpg')] w-full bg-cover opacity-75 flex items-center justify-center"
+  >
+    <div class="opacity-100 text-white text-center">
+      <h1 class="text-6xl">UK Barcode of Life</h1>
+      <h2 class="text-3xl">
+        Insert some pithy text about how great the project is here...
+      </h2>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
