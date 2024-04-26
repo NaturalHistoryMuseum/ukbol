431100cee70321c930a99def436edeba45365b98
Josh Humphries <j.humphries@nhm.ac.uk>
2024-04-26 16:41:46 +0100

    build: add another database to the db for tests

---
+++ docker/db/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# create a test database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE test;
+	GRANT ALL PRIVILEGES ON DATABASE test TO "$POSTGRES_USER";
+EOSQL
