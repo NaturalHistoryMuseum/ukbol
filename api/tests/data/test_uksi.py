b8c8f1d3428b04f6ad5340b31da0ec09b4baf8a2
Josh Humphries <j.humphries@nhm.ac.uk>
2024-04-27 17:15:31 +0100

    test: refactor uksi tests

--- api/tests/data/test_uksi.py
+++ api/tests/data/test_uksi.py
@@ -6,50 +6,50 @@ from ukbol.model import Taxon, Synonym
 uksi_dwca = Path(__file__).parent.parent / "files" / "uksi_dwca.zip"
 
 
-def test_rebuild_uksi_tables(app):
-    rebuild_uksi_tables(uksi_dwca)
+class TestRebuildUKSITables:
+    def test_basic(self, app):
+        rebuild_uksi_tables(uksi_dwca)
 
-    assert Taxon.query.count() == 16
-    assert Synonym.query.count() == 4
+        assert Taxon.query.count() == 16
+        assert Synonym.query.count() == 4
 
-    biota = Taxon.query.get("NHMSYS0021048735")
-    assert biota.name == "biota"
-    assert biota.rank == "unranked"
-    assert biota.parent_id is None
-    assert len(biota.children) == 1
+        biota = Taxon.query.get("NHMSYS0021048735")
+        assert biota.name == "biota"
+        assert biota.rank == "unranked"
+        assert biota.parent_id is None
+        assert len(biota.children) == 1
 
-    eukaryota = biota.children[0]
-    assert eukaryota.id == "NBNSYS0100003095"
-    assert eukaryota.name == "eukaryota"
-    assert eukaryota.rank == "domain"
+        eukaryota = biota.children[0]
+        assert eukaryota.id == "NBNSYS0100003095"
+        assert eukaryota.name == "eukaryota"
+        assert eukaryota.rank == "domain"
 
-    fungi = Taxon.query.get("NHMSYS0020535450")
-    assert fungi.name == "fungi"
-    assert fungi.rank == "kingdom"
-    assert len(fungi.children) == 2
-    ascomycota, zygomycota = sorted(fungi.children, key=lambda t: t.name)
-    assert ascomycota.name == "ascomycota"
-    assert zygomycota.name == "zygomycota"
+        fungi = Taxon.query.get("NHMSYS0020535450")
+        assert fungi.name == "fungi"
+        assert fungi.rank == "kingdom"
+        assert len(fungi.children) == 2
+        ascomycota, zygomycota = sorted(fungi.children, key=lambda t: t.name)
+        assert ascomycota.name == "ascomycota"
+        assert zygomycota.name == "zygomycota"
 
-    abrothallus_cetrariae = Taxon.query.get("BMSSYS0000000001")
-    assert abrothallus_cetrariae.parent == Taxon.query.get("NHMSYS0001472727")
-    assert sorted(abrothallus_cetrariae.synonyms, key=lambda s: s.id) == [
-        Synonym.query.get("BMSSYS0000042050"),
-        Synonym.query.get("NHMSYS0000361124"),
-    ]
-    assert Synonym.query.get("BMSSYS0000000016").rank == "variety"
+        abrothallus_cetrariae = Taxon.query.get("BMSSYS0000000001")
+        assert abrothallus_cetrariae.parent == Taxon.query.get("NHMSYS0001472727")
+        assert sorted(abrothallus_cetrariae.synonyms, key=lambda s: s.id) == [
+            Synonym.query.get("BMSSYS0000042050"),
+            Synonym.query.get("NHMSYS0000361124"),
+        ]
+        assert Synonym.query.get("BMSSYS0000000016").rank == "variety"
 
+    def test_with_old_data(self, app):
+        # add a load of data
+        rebuild_uksi_tables(uksi_dwca)
+        first_taxon_count = Taxon.query.count()
+        first_synonym_count = Synonym.query.count()
 
-def test_rebuild_old_data(app):
-    # add a load of data
-    rebuild_uksi_tables(uksi_dwca)
-    first_taxon_count = Taxon.query.count()
-    first_synonym_count = Synonym.query.count()
-
-    # then add the data again, this should delete what was added a moment ago and
-    # then add it again
-    rebuild_uksi_tables(uksi_dwca)
-    second_taxon_count = Taxon.query.count()
-    second_synonym_count = Synonym.query.count()
-    assert first_taxon_count == second_taxon_count
-    assert first_synonym_count == second_synonym_count
+        # then add the data again, this should delete what was added a moment ago and
+        # then add it again
+        rebuild_uksi_tables(uksi_dwca)
+        second_taxon_count = Taxon.query.count()
+        second_synonym_count = Synonym.query.count()
+        assert first_taxon_count == second_taxon_count
+        assert first_synonym_count == second_synonym_count
