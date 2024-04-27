3b9899d7a28df76641e7f75d16d3cd9ab89e4c52
Josh Humphries <j.humphries@nhm.ac.uk>
2024-04-27 17:49:35 +0100

    style: reformat status route

--- api/ukbol/routes/status.py
+++ api/ukbol/routes/status.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-blueprint = Blueprint('status', __name__)
+blueprint = Blueprint("status", __name__)
 
 
-@blueprint.route('/status')
+@blueprint.route("/status")
 def status() -> dict:
     return {"status": ":)"}
