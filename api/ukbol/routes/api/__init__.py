dfa125aec85a5f6f6364bc27c8da222b7eacfd9c
Josh Humphries <j.humphries@nhm.ac.uk>
2024-04-30 18:44:06 +0100

    refactor: move /status to /api/status

--- api/ukbol/routes/api/__init__.py
+++ api/ukbol/routes/api/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
-from ukbol.routes.api import taxon
+from ukbol.routes.api import taxon, status
 
 
 def bind_api_routes(app: Flask):
-    app.register_blueprint(taxon.blueprint, url_prefix="/api")
+    url_prefix = "/api"
+    app.register_blueprint(status.blueprint, url_prefix=url_prefix)
+    app.register_blueprint(taxon.blueprint, url_prefix=url_prefix)
