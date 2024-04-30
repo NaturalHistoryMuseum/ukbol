5dd07718aada6080901dac071314f0e8338cd8c8
Josh Humphries <j.humphries@nhm.ac.uk>
2024-04-30 11:45:38 +0100

    chore: add api proxy for dev

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      // for the dev env, proxy the requests to the API to the Flask server
+      '/api': 'http://localhost:5000',
+    },
+  },
 });
