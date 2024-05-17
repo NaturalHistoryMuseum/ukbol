27e4282dacd878f7ca6c40579298ddeb381cab79
Josh Humphries <j.humphries@nhm.ac.uk>
2024-05-17 12:55:17 +0100

    feat: add basics of the data page

--- web/src/components/Data.vue
+++ web/src/components/Data.vue
@@ -1,7 +1,32 @@
 <template>
-  <h1>Data!</h1>
+  <div class="data-height flex w-full gap-4 h-screen">
+    <div class="w-1/5 bg-slate-200 flex flex-col">
+      <div>
+        <input type="search" placeholder="Search..." class="p-1 w-full" />
+      </div>
+      <div class="overflow-y-auto p-1">
+        <TreeNode v-for="root in roots" :taxon="root" :depth="0"></TreeNode>
+      </div>
+    </div>
+    <div class="w-4/5 bg-slate-500"></div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import TreeNode from './TreeNode.vue';
+import axios from 'axios';
+import { ref } from 'vue';
 
-<style scoped></style>
+const roots = ref([]);
+
+// retrieve the root taxa from the API
+axios
+  .get('/api/taxon/roots')
+  .then((response) => roots.value.push(...response.data));
+</script>
+
+<style scoped>
+.data-height {
+  height: calc(100vh - var(--nav-height) - var(--foot-height));
+}
+</style>
---
+++ web/src/components/TreeNode.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul>
+    <li>
+      <div
+        @click="onClick"
+        class="cursor-pointer"
+        :style="{ paddingLeft: `${depth * 10}px` }"
+      >
+        <span :class="nameClass">{{ taxon.name }}</span>
+        <FontAwesomeIcon
+          v-if="taxon.children.length > 0"
+          :icon="faCaretRight"
+        />
+      </div>
+
+      <TreeNode
+        v-for="childTaxon in childTaxa"
+        :taxon="childTaxon"
+        :depth="depth + 1"
+      >
+      </TreeNode>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { computed, ref } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+
+const { taxon } = defineProps(['taxon', 'depth']);
+const childTaxa = ref([]);
+
+const nameClass = computed(() => {
+  let base = ['capitalize'];
+  if (['genus', 'species', 'subspecies', 'variety'].includes(taxon.rank)) {
+    base.push('italic');
+  }
+  return base;
+});
+
+const onClick = () => {
+  console.log(`Selected ${taxon.name} [${taxon.rank}] ${taxon.id}`);
+  if (taxon.children.length > 0) {
+    if (childTaxa.value.length === 0) {
+      axios
+        .get(`/api/taxon/${taxon.id}/children`)
+        .then((response) => childTaxa.value.push(...response.data))
+        .catch(console.log);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
