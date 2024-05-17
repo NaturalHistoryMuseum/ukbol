492463d4083a52825ea97e863c91bde52cabd9e1
Josh Humphries <j.humphries@nhm.ac.uk>
2024-05-17 20:09:47 +0100

    feat: add states to control node functionality

--- web/src/components/tree/Node.vue
+++ web/src/components/tree/Node.vue
@@ -6,14 +6,12 @@
         class="cursor-pointer"
         :style="{ paddingLeft: `${depth * 10}px` }"
       >
-        <span :class="nameClass">{{ taxon.name }}</span>
-        <FontAwesomeIcon
-          v-if="taxon.children.length > 0"
-          :icon="faCaretRight"
-        />
+        <span class="pr-1.5" :class="nameClass">{{ taxon.name }}</span>
+        <FontAwesomeIcon v-if="state !== State.Leaf" :icon="nodeIcon" />
       </div>
 
       <Node
+        v-if="state === State.Expanded"
         v-for="childTaxon in childTaxa"
         :taxon="childTaxon"
         :depth="depth + 1"
@@ -27,10 +25,17 @@
 import axios from 'axios';
 import { computed, ref } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { faSquareMinus, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+
+const State = Object.freeze({
+  Leaf: 'Leaf',
+  Expanded: 'Expanded',
+  Collapsed: 'Collapsed',
+});
 
 const { taxon } = defineProps(['taxon', 'depth']);
 const childTaxa = ref([]);
+const state = ref(taxon.children.length === 0 ? State.Leaf : State.Collapsed);
 
 const nameClass = computed(() => {
   let base = ['capitalize'];
@@ -40,15 +45,36 @@ const nameClass = computed(() => {
   return base;
 });
 
+const nodeIcon = computed(() => {
+  switch (state.value) {
+    case State.Expanded:
+      return faSquareMinus;
+    case State.Collapsed:
+      return faSquarePlus;
+  }
+});
+
 const onClick = () => {
   console.log(`Selected ${taxon.name} [${taxon.rank}] ${taxon.id}`);
-  if (taxon.children.length > 0) {
-    if (childTaxa.value.length === 0) {
-      axios
-        .get(`/api/taxon/${taxon.id}/children`)
-        .then((response) => childTaxa.value.push(...response.data))
-        .catch(console.log);
-    }
+
+  switch (state.value) {
+    case State.Leaf:
+      // if it's a species, change the data view
+      break;
+    case State.Expanded:
+      // collapse the tree
+      state.value = State.Collapsed;
+      break;
+    case State.Collapsed:
+      // expand the tree and load if needed
+      if (taxon.children.length > 0 && childTaxa.value.length === 0) {
+        axios
+          .get(`/api/taxon/${taxon.id}/children`)
+          .then((response) => childTaxa.value.push(...response.data))
+          .catch(console.log);
+      }
+      state.value = State.Expanded;
+      break;
   }
 };
 </script>
