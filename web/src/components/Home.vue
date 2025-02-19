39fdd8b70ef70cd051c6d0f20ac066fb4268540c
Josh Humphries <j.humphries@nhm.ac.uk>
2025-02-19 16:10:29 +0000

    ui: change home background and tweak font colour to match

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -4,21 +4,21 @@
   >
     <div class="text-slate-100 text-center">
       <h1 class="text-7xl bold">UK Barcode of Life</h1>
-      <h2 class="text-2xl pb-10 text-slate-900">
+      <h2 class="text-2xl pb-10">
         DNA barcoding the biota of the United Kingdom to assemble and curate a
         comprehensive library of standardized DNA sequences
       </h2>
       <div class="flex justify-stretch text-lg">
         <router-link
           to="about"
-          class="flex-1 text-right hover:text-slate-200 font-bold text-slate-50 p-2"
+          class="flex-1 text-right hover:text-slate-200 font-bold text-white p-2"
         >
           About
         </router-link>
         <span class="p-2 font-bold">|</span>
         <router-link
           to="data"
-          class="flex-1 text-left hover:text-slate-200 font-bold text-slate-50 p-2"
+          class="flex-1 text-left hover:text-slate-200 font-bold text-white p-2"
         >
           Browse
         </router-link>
@@ -26,8 +26,11 @@
     </div>
     <div class="absolute right-0 bottom-0 p-1 text-sm text-white">
       Photo by
-      <a href="https://unsplash.com/@mangofantasy">Tim Johnson</a> on
-      <a href="https://unsplash.com/photos/green-leaf-CHv5D04_GWk">Unsplash</a>
+      <a href="https://unsplash.com/@boardshortsben">Benjamin L. Jones</a> on
+      <a
+        href="https://unsplash.com/photos/blue-and-black-abstract-painting-gu5AAPt4pew"
+        >Unsplash</a
+      >
     </div>
   </div>
 </template>
